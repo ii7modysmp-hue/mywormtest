17a2ffa1bd9ecc8ba41682ad6e74dca1746b4e8d
Abso <ii7modysmp@gmail.com>
2026-03-22 10:27:24 +0300

    Create user.php

---
+++ api/user.php
@@ -0,0 +1,12 @@
+{
+    "success": true,
+    "Users": [
+        {
+            "id": 1,
+            "client_name": " test worm ",
+            "client_ID": "gg_106263452150533879793",
+            "client_DateExpired": "2033-12-22",
+            "status": 1
+        }
+    ]
+}    
